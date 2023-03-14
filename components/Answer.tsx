2f4da93a20411ee65acddc0cf4341158f2924a49
kellemar <decruz@hey.com>
2023-03-15 02:46:10 +0800

    added stored messages state

--- components/Answer.tsx
+++ components/Answer.tsx
@@ -4,88 +4,88 @@ import { useRef } from 'react';
 import { link } from "fs";
 import ReactMarkdown from 'react-markdown'
 
+const games = ["Phantom Galaxies", "Illuvium", "League of Kingdoms", "Decentraland", "Apeiron", "Star Atlas"]
 
+type Answer = {
+  id: number,
+  question: string,
+  output: string
+};
 
 type AnswerPropsType = {
-  generatedAnswer: string,
+  outputAnswer: Array<Answer>,
 };
 
 
 
-export default function Answer(this: any, { generatedAnswer }: AnswerPropsType) {
+export default function Answer({ outputAnswer }: AnswerPropsType) {
+
+  //const baseURL = "https://core.avocadodao.io/games/";
+  //const answer = JSON.parse(generatedAnswer)['output'];
+  //const video = JSON.parse(generatedAnswer)['video'];
+  //const articleLink = JSON.parse(generatedAnswer)['article'];
 
   const baseURL = "https://core.avocadodao.io/games/";
-  const answer = JSON.parse(generatedAnswer)['output'];
-  const video = JSON.parse(generatedAnswer)['video'];
-  const articleLink = JSON.parse(generatedAnswer)['article'];
-  const windowSize = useRef([window.innerWidth, window.innerHeight]);
+  const video = "";
+  const articleLink = "";
 
-  // Check for possible lists being presented
-  if (answer.includes(". -")){
+  const listItems = outputAnswer.map(({ id, question, output }) =>
+    <>
+      <div>
+      </div>
+      <div className="space-y-2 flex flex-col items-center justify-center min-w-m max-w-xl mx-auto max-w-xl w-full">
+      <div
+            className="mt-5 ml-auto p-10 bg-purple-200 rounded-xl text-black text-s px-2 mb-2 py-2">
+            {question}
+          </div>
+        <div className="bg-purple-100 rounded-xl shadow-md p-4 hover:bg-purple-200 transition border " key={id}>
+        
+          <ReactMarkdown className="text-left m-5">{output}</ReactMarkdown>
+          {articleLink && (
+            <div className="text-left m-5">
+              Read more at: {" "}
+              <a
+                href={baseURL + articleLink}
+                target="_blank"
+                rel="noreferrer"
+                className="font-bold hover:underline transition underline-offset-2"
+              >
+                {baseURL + articleLink}
+              </a>
 
-  }
+            </div>)}
+          {video && (
 
-  const opts = {
-  
-   playerVars: {
-      autoplay: 0,
-    },
-  };
+            <div className="flex mt-10 justify-center">
+              <iframe src={"https://www.youtube.com/embed/" + video} className="w-full aspect-video" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
+            </div>
+
+          )}
+          <button
+            className="mt-5 ml-auto p-10 bg-purple-400 rounded-xl text-white text-xs px-2 mb-2 py-2 hover:bg-purple-500"
+            onClick={() => {
+              navigator.clipboard.writeText(output);
+              toast("Text copied!", {
+                icon: "✂️",
+              });
+            }}>
+            Copy Text
+          </button>
+        </div>
+
+
+      </div>
+    </>
+
+  );
   return (
     <>
-    <Toaster
-          position="top-center"
-          reverseOrder={false}
-          toastOptions={{ duration: 2000 }}
-        />
-    <div>
-                <h2
-                  className="sm:text-2xl text-2xl font-bold text-slate-900 mx-auto"
-                  >
-                  Here's what we have
-                </h2>
-                
-              </div>
-              <div className="space-y-8 flex flex-col items-center justify-center min-w-m max-w-xl mx-auto max-w-xl w-full">
-              
-              <div className="bg-purple-100 rounded-xl shadow-md p-4 hover:bg-purple-200 transition border "
-                        
-                        key={answer}>
-            
-              <button
-              className="ml-auto p-10 bg-purple-400 rounded-xl text-white text-xs px-2 mb-2 py-2 hover:bg-purple-500"
-              onClick={() => {
-                navigator.clipboard.writeText(answer);
-                toast("Text copied!", {
-                  icon: "✂️",
-                });
-              }}>
-              Copy Text
-            </button>
-                          <ReactMarkdown className="text-left m-5">{answer}</ReactMarkdown>
-                          {articleLink && (
-          <div className="text-left m-5">
-        Read more at: {" "}
-        <a
-          href={baseURL+articleLink}
-          target="_blank"
-          rel="noreferrer"
-          className="font-bold hover:underline transition underline-offset-2"
-        >
-          {baseURL+articleLink}
-        </a>
-        
-      </div>)}
-                      {video && (
-                        
-                          <div className="flex mt-10 justify-center">
-                            <iframe src={"https://www.youtube.com/embed/"+video} className="w-full aspect-video" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
-                          </div>
-
-                      )}
-                      </div>
-                
-              </div>
-              </>
+      <Toaster
+        position="top-center"
+        reverseOrder={false}
+        toastOptions={{ duration: 2000 }}
+      />
+      {listItems}
+    </>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,7 @@ import Footer from "../components/Footer";
 import Header from "../components/Header";
 import Answer from "../components/Answer";
 import LoadingDots from "../components/LoadingDots";
+import { text } from "stream/consumers";
 
 
 const games = ["Phantom Galaxies", "Illuvium", "League of Kingdoms", "Decentraland", "Apeiron", "Star Atlas"]
@@ -14,25 +15,28 @@ const Home: NextPage = () => {
 
   const [gameName, setGameName] = useState("");
   const [loading, setLoading] = useState(false);
-  const [answer, setAnswer] = useState("");
-  const [generatedAnswer, setGeneratedAnswer] = useState<String>("");
+  const [question, setQuestion] = useState("");
+  const [answerList, updateAnswerList] = useState<any[]>([]);
+  const [generatedAnswer, setGeneratedAnswer] = useState("");
   const [timeTaken, setTimeTaken] = useState("");
   const auth_key = process.env.NEXT_PUBLIC_AUTH_KEY || "NA";
   const api_url = process.env.NEXT_PUBLIC_SERVER_API_URL || "NA";
 
+  const [textCount, setTextCount] = useState(0);
+
   const answerRef = useRef<null | HTMLDivElement>(null);
-  
+
   const scrollToBios = () => {
     if (answerRef.current !== null) {
       answerRef.current.scrollIntoView({ behavior: "smooth" });
     }
   };
 
-  const input = answer;
-  
+  const input = question;
+
   useEffect(() => {
     const randomNumber = Math.floor(Math.random() * games.length);
-    setGameName("What is "+games[randomNumber]);
+    setGameName("What is " + games[randomNumber]);
   }, []);
 
   const generateAnswer = async (e: any) => {
@@ -55,29 +59,19 @@ const Home: NextPage = () => {
     if (!response.ok) {
       throw new Error(response.statusText);
     }
+    const result = await response.json();
 
-    // This data is a ReadableStream
-    const data = response.body;
-    if (!data) {
-      return;
-    }
-    
-    const reader = data.getReader();
-    const decoder = new TextDecoder();
-    let done = false;
 
-    while (!done) {
-      const { value, done: doneReading } = await reader.read();
-      done = doneReading;
-      if(done == true){
-        setTimeTaken((((Date.now() - startTime) % 60000) / 1000) + "");
-        
-      }
-      const chunkValue = decoder.decode(value);
-      setGeneratedAnswer((prev) => prev + chunkValue);
-    }
+    setGeneratedAnswer(result);
     scrollToBios();
     setLoading(false);
+
+    const outputOnly = result['output'];
+    setTextCount(textCount + 1);
+    updateAnswerList([...answerList, { "id": textCount, "question": input, "output": outputOnly }]);
+    setTextCount(textCount + 1);
+    setQuestion("");
+
   };
 
   return (
@@ -94,58 +88,58 @@ const Home: NextPage = () => {
         </h1>
         <div className="max-w-xl w-full">
           <div className="flex items-center justify-center space-x-3">
-          <button onClick={(e) => setAnswer(e.currentTarget.innerHTML)}
+            <button onClick={(e) => setQuestion(e.currentTarget.innerHTML)}
               className="bg-purple-400 rounded-xl text-white text-xs px-4 py-2 sm:mt-10 mt-8 hover:bg-black/80 w-full h-20 sm:h-12">
               What are the games available?
-              </button>
-              <button onClick={(e) => setAnswer(e.currentTarget.innerHTML)}
+            </button>
+            <button onClick={(e) => setQuestion(e.currentTarget.innerHTML)}
               className="bg-purple-400 rounded-xl text-white text-xs px-4 py-2 sm:mt-10 mt-8 hover:bg-black/80 w-full h-20 sm:h-12">
               {gameName}?
-              </button>
-              <button onClick={(e) => setAnswer(e.currentTarget.innerHTML)}
+            </button>
+            <button onClick={(e) => setQuestion(e.currentTarget.innerHTML)}
               className="bg-purple-400 rounded-xl text-white text-xs px-4 py-2 sm:mt-10 mt-8 hover:bg-black/80 w-full h-20 sm:h-12">
               What are the action games?
-              </button>
+            </button>
           </div>
+          <div className="space-y-5 my-2" ref={answerRef}>
+            {answerList && (
+              <>
+                <Answer outputAnswer={answerList} />
+              </>
+            )}
+          </div>
+          {timeTaken && (
+            <div className="text-sm">
+              <p>This answer took {timeTaken} seconds.</p>
+            </div>
+          )}
           <textarea
-            value={answer}
-            onChange={(e) => setAnswer(e.target.value)}
+            value={question}
+            onChange={(e) => setQuestion(e.target.value)}
             rows={4}
             className="w-full rounded-md border-gray-300 shadow-sm focus:border-black focus:ring-black my-5"
             placeholder={
-              "e.g. What is Phantom Galaxies? \nWhat is the token price of Phantom Galaxies?\nWho is the team behind Phantom Galaxies?"
+              "What's your question?"
             }
           />
           {!loading && (
             <button
-              className="bg-black rounded-xl text-white font-medium px-4 py-2 sm:mt-10 mt-8 hover:bg-black/80 w-full"
+              className="bg-black rounded-xl text-white font-medium px-4 py-2 mt-2 hover:bg-black/80 w-full"
               onClick={(e) => generateAnswer(e)}>
               Ask!
             </button>
           )}
           {loading && (
             <button
-              className="bg-black rounded-xl text-white font-medium px-4 py-2 sm:mt-10 mt-8 hover:bg-black/80 w-full"
+              className="bg-black rounded-xl text-white font-medium px-4 py-2 mt-2 hover:bg-black/80 w-full"
               disabled
             >
               <LoadingDots color="white" style="large" />
             </button>
           )}
         </div>
-        
-        <hr className="h-px bg-gray-700 border-1 dark:bg-gray-700" />
-        <div className="space-y-5 my-5" ref={answerRef}>
-          {generatedAnswer && (
-            <>
-              <Answer generatedAnswer={generatedAnswer.toString()}/>
-            </>
-          )}
-        </div>
-        {timeTaken && (
-        <div className="text-sm">
-           <p>This answer took {timeTaken} seconds.</p>
-        </div>
-        )}
+
+
       </main>
       <Footer />
     </div>
